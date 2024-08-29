c4cad585794e49e2b369da7c5a3aecb553a2950e
Clay Schuller <cschuller2@huskers.unl.edu>
2024-08-29 12:40:25 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Name: Clay Schuller
+ * Email: Cschuller2@huskers.unl.edu
+ * Purpose: Print my name
+ * Date:8/29/2024
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    printf("Clay Schuller\n");
+
+}
+
+
+
+
